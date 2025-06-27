b531257863f130cb612b8dad56afbb03040644fa
sanket-forge <sanketkhatua9861@gmail.com>
2025-06-28 01:20:26 +0530

    Initial Commit

---
+++ AuthFirebase/AuthFirebaseApp.swift
@@ -0,0 +1,31 @@
+//
+//  AuthFirebaseApp.swift
+//  AuthFirebase
+//
+//  Created by Sanket Khatua on 28/06/25.
+//
+
+import SwiftUI
+import FirebaseCore
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+@main
+struct AuthFirebaseApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            AuthView()
+        }
+    }
+}
